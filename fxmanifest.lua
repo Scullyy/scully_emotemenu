d68b85eba1ff8ce7d1fd009093166d24aa09c476
Scully <51968381+Scullyy@users.noreply.github.com>
2025-01-22 10:59:17 -0800

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -49,3 +49,21 @@ files {
     'client/modules/ragdoll.lua',
     'client/modules/stance.lua'
 }
+
+data_file 'DLC_ITYP_REQUEST' 'bzzz_foodpack.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'bzzz_prop_torch_fire001.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'natty_props_lollipops.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'scully_props.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'badges.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'lilflags_ytyp.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'prideprops_ytyp.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'bzzz_food_icecream_pack.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'bzzz_effect_cigarpack.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'bzzz_murderpack.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'bzzz_animal_fish002.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'bzzz_food_dessert_a.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'bzzz_prop_give_gift.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'bzzz_package_pizzahut.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'ultra_ringcase.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'bzzz_food_xmas22.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'pprp_icefishing.ytyp'
