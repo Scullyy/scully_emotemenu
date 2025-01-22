45ded455016b6e818929f81f5d7b3f777021f235
Scully <51968381+Scullyy@users.noreply.github.com>
2025-01-22 00:04:57 -0800

    Fix expression names and add missing ones

--- shared/data/expressions.lua
+++ shared/data/expressions.lua
@@ -5,9 +5,19 @@ return {
         Expression = 'mood_aiming_1',
     },
     {
+        Label = 'Aiming 2',
+        Command = 'aiming2',
+        Expression = 'pose_aiming_1',
+    },
+    {
         Label = 'Angry',
         Command = 'angry',
         Expression = 'mood_angry_1',
+    },
+    {
+        Label = 'Angry 2',
+        Command = 'angry2',
+        Expression = 'pose_angry_1',
     },
     {
         Label = 'Burning',
@@ -15,9 +25,54 @@ return {
         Expression = 'burning_1',
     },
     {
+        Label = 'Coughing',
+        Command = 'coughing',
+        Expression = 'coughing_1',
+    },
+    {
         Label = 'Crying',
         Command = 'crying',
         Expression = 'console_wasnt_fun_end_loop_floyd_facial',
+    },
+    {
+        Label = 'Dancing',
+        Command = 'dancing',
+        Expression = 'mood_dancing_low_1',
+    },
+    {
+        Label = 'Dancing 2',
+        Command = 'dancing2',
+        Expression = 'mood_dancing_low_2',
+    },
+    {
+        Label = 'Dancing 3',
+        Command = 'dancing3',
+        Expression = 'mood_dancing_low_3',
+    },
+    {
+        Label = 'Dancing 4',
+        Command = 'dancing4',
+        Expression = 'mood_dancing_medium_1',
+    },
+    {
+        Label = 'Dancing 5',
+        Command = 'dancing5',
+        Expression = 'mood_dancing_medium_2',
+    },
+    {
+        Label = 'Dancing 6',
+        Command = 'dancing6',
+        Expression = 'mood_dancing_medium_3',
+    },
+    {
+        Label = 'Dancing 7',
+        Command = 'dancing7',
+        Expression = 'mood_dancing_high_1',
+    },
+    {
+        Label = 'Dancing 8',
+        Command = 'dancing8',
+        Expression = 'mood_dancing_high_2',
     },
     {
         Label = 'Dead',
@@ -30,19 +85,64 @@ return {
         Expression = 'dead_2',
     },
     {
+        Label = 'Die',
+        Command = 'die',
+        Expression = 'die_1',
+    },
+    {
+        Label = 'Die 2',
+        Command = 'die2',
+        Expression = 'die_2',
+    },
+    {
+        Label = 'Drinking',
+        Command = 'drinking',
+        Expression = 'drinking_1',
+    },
+    {
+        Label = 'Drive Fast',
+        Command = 'drivefast',
+        Expression = 'mood_drivefast_1',
+    },
+    {
         Label = 'Drunk',
         Command = 'drunk',
         Expression = 'mood_drunk_1',
     },
     {
-        Label = 'Dumb',
-        Command = 'dumb',
-        Expression = 'pose_injured_1',
+        Label = 'Eating',
+        Command = 'eating',
+        Expression = 'eating_1',
     },
     {
         Label = 'Effort',
         Command = 'effort',
         Expression = 'effort_1',
+    },
+    {
+        Label = 'Effort 2',
+        Command = 'effort2',
+        Expression = 'effort_2',
+    },
+    {
+        Label = 'Effort 3',
+        Command = 'effort3',
+        Expression = 'effort_3',
+    },
+    {
+        Label = 'Effort 4',
+        Command = 'effort4',
+        Expression = 'melee_effort_1',
+    },
+    {
+        Label = 'Effort 5',
+        Command = 'effort5',
+        Expression = 'melee_effort_2',
+    },
+    {
+        Label = 'Effort 6',
+        Command = 'effort6',
+        Expression = 'melee_effort_3',
     },
     {
         Label = 'Electrocuted',
@@ -60,24 +160,24 @@ return {
         Expression = 'mood_frustrated_1',
     },
     {
-        Label = 'Grumpy',
-        Command = 'grumpy',
-        Expression = 'effort_1',
-    },
-    {
-        Label = 'Grumpy 2',
-        Command = 'grumpy2',
-        Expression = 'mood_drivefast_1',
-    },
-    {
-        Label = 'Grumpy 3',
-        Command = 'grumpy3',
-        Expression = 'pose_angry_1',
-    },
-    {
         Label = 'Happy',
         Command = 'happy',
         Expression = 'mood_happy_1',
+    },
+    {
+        Label = 'Happy 2',
+        Command = 'happy2',
+        Expression = 'pose_happy_1',
+    },
+    {
+        Label = 'High',
+        Command = 'high',
+        Expression = 'high_transition_01',
+    },
+    {
+        Label = 'High 2',
+        Command = 'high2',
+        Expression = 'high_transition_02',
     },
     {
         Label = 'Injured',
@@ -85,24 +185,54 @@ return {
         Expression = 'mood_injured_1',
     },
     {
-        Label = 'Joyful',
-        Command = 'joyful',
-        Expression = 'mood_dancing_low_1',
+        Label = 'Injured 2',
+        Command = 'injured 2',
+        Expression = 'pose_injured_1',
     },
     {
-        Label = 'Mouth breather',
-        Command = 'mouthbreather',
-        Expression = 'smoking_hold_1',
+        Label = 'Knockout',
+        Command = 'knockout',
+        Expression = 'mood_knockout_1',
     },
     {
-        Label = 'Never Blink',
-        Command = 'neverblink',
+        Label = 'Normal',
+        Command = 'normal',
+        Expression = 'mood_normal_1',
+    },
+    {
+        Label = 'Normal 2',
+        Command = 'normal2',
         Expression = 'pose_normal_1',
     },
     {
-        Label = 'One Eye',
-        Command = 'oneeye',
-        Expression = 'pose_aiming_1',
+        Label = 'Pain',
+        Command = 'pain',
+        Expression = 'pain_1',
+    },
+    {
+        Label = 'Pain 2',
+        Command = 'pain2',
+        Expression = 'pain_2',
+    },
+    {
+        Label = 'Pain 3',
+        Command = 'pain3',
+        Expression = 'pain_3',
+    },
+    {
+        Label = 'Pain 4',
+        Command = 'pain4',
+        Expression = 'pain_4',
+    },
+    {
+        Label = 'Pain 5',
+        Command = 'pain5',
+        Expression = 'pain_5',
+    },
+    {
+        Label = 'Pain 6',
+        Command = 'pain6',
+        Expression = 'pain_6',
     },
     {
         Label = 'Shocked',
@@ -115,9 +245,29 @@ return {
         Expression = 'shocked_2',
     },
     {
+        Label = 'Skydive',
+        Command = 'skydive',
+        Expression = 'mood_skydive_1',
+    },
+    {
         Label = 'Sleeping',
         Command = 'sleeping',
         Expression = 'mood_sleeping_1',
+    },
+    {
+        Label = 'Smoking',
+        Command = 'smoking',
+        Expression = 'smoking_exhale_1',
+    },
+    {
+        Label = 'Smoking 2',
+        Command = 'smoking2',
+        Expression = 'smoking_hold_1',
+    },
+    {
+        Label = 'Smoking 3',
+        Command = 'smoking3',
+        Expression = 'smoking_inhale_1',
     },
     {
         Label = 'Smug',
@@ -125,9 +275,9 @@ return {
         Expression = 'mood_smug_1',
     },
     {
-        Label = 'Speculative',
-        Command = 'speculative',
-        Expression = 'mood_aiming_1',
+        Label = 'Smug 2',
+        Command = 'smug2',
+        Expression = 'pose_smug_1',
     },
     {
         Label = 'Stressed',
@@ -135,18 +285,18 @@ return {
         Expression = 'mood_stressed_1',
     },
     {
-        Label = 'Sulking',
-        Command = 'sulking',
+        Label = 'Stressed 2',
+        Command = 'stressed2',
+        Expression = 'pose_stressed_1',
+    },
+    {
+        Label = 'Sulk',
+        Command = 'sulk',
         Expression = 'mood_sulk_1',
     },
     {
-        Label = 'Weird',
-        Command = 'weird',
-        Expression = 'effort_2',
-    },
-    {
-        Label = 'Weird 2',
-        Command = 'weird2',
-        Expression = 'effort_3',
+        Label = 'Sulk 2',
+        Command = 'sulk2',
+        Expression = 'pose_sulk_1',
     }
 }
