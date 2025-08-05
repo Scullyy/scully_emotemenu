9de295e05c5934d05e6a55085d21efb9de3fef31
0Programmer <60280452+0Programmer@users.noreply.github.com>
2025-08-05 03:25:09 +0200

    fix(client/modules): fix handsup cancel on toggle, reset anim on ragdoll and added disabled steering while hands are up (#106)
    * fix(client/modules): fix handsup, reset anim and disabled steering
    
    * chore(client/modules): changed needsReset value
    
    * chore(client/modules): reset anim only after ragdoll
    
    * chore(client/modules): styling request

--- client/modules/handsup.lua
+++ client/modules/handsup.lua
@@ -3,7 +3,7 @@ lib.addKeybind({
     description = locale('hands_up'),
     defaultKey = Config.handsUpKey,
     onPressed = function()
-        if Config.handsUpIsToggle and PlayerState.handsup then PlayerState.handsup = false end
+        if Config.handsUpIsToggle and PlayerState.handsup then PlayerState.handsup = false return end
         if PlayerState.isLimited then return end
         if GetVehiclePedIsEntering(cache.ped) ~= 0 then return end
 
@@ -20,15 +20,23 @@ lib.addKeybind({
 
         PlayerState.handsup = true
 
+        local needsReset = false
         CreateThread(function()
             while PlayerState.handsup do
                 Wait(0)
 
+                if cache.vehicle then
+                    DisableControlAction(0, 59, true)
+                end
+
                 DisableControlAction(0, 25, true)
                 DisablePlayerFiring(cache.playerId, true)
 
-                if Config.handsUpIsToggle and not IsEntityPlayingAnim(cache.ped, 'random@mugging3', 'handsup_standing_base', 3) then
+                if IsPedRagdoll(cache.ped) then needsReset = true end
+
+                if (Config.handsUpIsToggle and not IsEntityPlayingAnim(cache.ped, 'random@mugging3', 'handsup_standing_base', 3)) or (needsReset and not IsPedRagdoll(cache.ped)) then
                     TaskPlayAnim(cache.ped, 'random@mugging3', 'handsup_standing_base', 8.0, 8.0, -1, 50, 0, false, onBike and 4127 or false, false)
+                    needsReset = false
                 end
             end
 
